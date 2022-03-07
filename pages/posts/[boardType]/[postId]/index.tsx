276298cee3cef4ada75478030ac73b3659895de9
andhl <andhlwhffu@gmail.com>
2022-03-07 15:55:08 +0900

    add useMemo useCallback

--- pages/posts/[boardType]/[postId]/index.tsx
+++ pages/posts/[boardType]/[postId]/index.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useCallback, useMemo } from "react";
 import { useQuery } from "react-query";
 import { api } from "../../../../api/api";
 import CommentForm from "../../../../components/CommentForm";
 import Comments from "../../../../components/Comments";
 import PageTitle from "../../../../components/PageTitle";
+import { ERROR, LOADING } from "../../../../constants/constants";
 import { useAppSelector } from "../../../../store/hooks";
 import { IDetailedPost } from "../../../../types/types";
 
@@ -14,23 +16,20 @@ export default function DetailedPostPage() {
 
   const token = useAppSelector((state) => state.auth.token);
 
+  const fetchDetailedPost = useCallback((): Promise<IDetailedPost> => {
+    return api.get(`/posts/${postId}`);
+  }, [postId]);
+
   const { data, status, error } = useQuery(
     ["posts", postId],
-    async () => {
-      const response: IDetailedPost = await api.get(`/posts/${postId}`);
-      return response;
-    },
+    fetchDetailedPost,
     {
       retry: false,
       enabled: !!token,
     }
   );
 
-  const isError = (error: unknown): error is Error => {
-    return error instanceof Error;
-  };
-
-  const postData = data?.data;
+  const postData = useMemo(() => data?.data, [data?.data]);
 
   return (
     <>
@@ -38,9 +37,9 @@ export default function DetailedPostPage() {
       <Link href={"/"}>
         <a>홈</a>
       </Link>
-      {status === "loading" ? (
+      {status === LOADING ? (
         <span>Loading</span>
-      ) : status === "error" && isError(error) ? (
+      ) : status === ERROR && error instanceof Error ? (
         <span>Error: {error.message}</span>
       ) : null}
       <div>
